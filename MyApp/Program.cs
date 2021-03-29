bb47fe34ab4a4ff21efd13256c6992613561b2bf
vimal <vimal@gmail.com>
2021-03-29 13:01:51 -0700

    Firsst Commit

---
+++ MyApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Hello World Testing");
+        }
+    }
+}
---
+++ UnitTest1/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.IO;
+using System;
+
+namespace MyApp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
